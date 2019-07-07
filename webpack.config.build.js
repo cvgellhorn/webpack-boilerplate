6df0f4a69295e47d41d640d02e9933efd66fdcc9
cvgellhorn <gellborn@gmail.com>
2019-07-07 21:39:39 +1000

    update plugin import

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const merge = require('webpack-merge');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpackConfig = require('./webpack.config');
 
 module.exports = merge(webpackConfig, {
