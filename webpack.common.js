9dee119cd8d614ec302a825177bd386369f31518
Christoph von Gellhorn <cvgellhorn@users.noreply.github.com>
2020-10-25 13:52:15 +1100

    change webpack env reading

--- webpack.common.js
+++ webpack.common.js
@@ -12,7 +12,11 @@ const dirAssets = path.join(__dirname, 'assets');
  */
 module.exports = env => {
     // Is the current build a development build
-    const IS_DEV = env === 'dev';
+    // const IS_DEV = env === 'dev';
+    const IS_DEV = !!env.dev;
+    console.log(env);
+    console.log(IS_DEV);
+    
     
     return {
         entry: {
